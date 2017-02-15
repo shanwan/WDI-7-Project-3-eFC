1b828ed6d8885c9b918e532fed3240dd8a6d679d
ShanWan <shan.wan.loo@gmail.com>
2017-02-15 11:29:37 +0800

    partials shows when clicked

--- app/models/patient_admission.rb
+++ app/models/patient_admission.rb
@@ -1,7 +1,6 @@
 class PatientAdmission < ApplicationRecord
   belongs_to :patient
-  # wardA = Ward.find_by_sql(Select * From wards WHERE ward_type = "A")
-  # wardA = Ward.find_by_sql(Select cost_per_day From wards WHERE ward_type = 'A')
+
   # i need to get the income of the patient who is logged in
   # i need the ward chosen ==> different forms rendered will therefore decide which ward chosen
   # i need to find the range that the income is in formulatable
