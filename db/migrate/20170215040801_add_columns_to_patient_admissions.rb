94285dbd5fa0ab843a5d1aa0338a59a65bdd69e1
ShanWan <shan.wan.loo@gmail.com>
2017-02-16 10:10:44 +0800

    resolves schema issue

--- db/migrate/20170215040801_add_columns_to_patient_admissions.rb
+++
@@ -1,6 +0,0 @@
-class AddColumnsToPatientAdmissions < ActiveRecord::Migration[5.0]
-  def change
-    add_column :PatientAdmissions, :claim_medisave, :integer
-    add_column :PatientAdmissions, :pay_cash, :integer
-  end
-end
